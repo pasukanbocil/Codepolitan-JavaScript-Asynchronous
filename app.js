bdda0b407a282e9f674e6c71736a9016ea5445e7
pasukanbocil <ctian8787@gmail.com>
2024-02-21 14:40:55 +0700

    Mbak M Jika Nanti Aku Tidak Bersamamu Aku Akan Sendirian Sampe Menemukan Wanita Sepertimu

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Bakal Muncul Pertama');
+
+setTimeout(()=>{
+    console.log('Ini udah masuk memory proses tapi dijalan setelah 4 detik');
+},3000);
+
+
+console.log('Bakal Muncul Kedua');
