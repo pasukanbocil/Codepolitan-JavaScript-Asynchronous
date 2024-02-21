fa9dc85327a495021f0ab0011b61ab31b2386410
pasukanbocil <ctian8787@gmail.com>
2024-02-21 15:12:09 +0700

    docs: :memo: promise

---
+++ promise/app.js
@@ -0,0 +1,33 @@
+// callback version
+const requestCallback = (url, success, failure) => {
+	const delay = Math.floor(Math.random() * 4500) + 500;
+	setTimeout(() => {
+		if (delay > 4000) {
+			failure('Error: Connection Timeout');
+		} else {
+			success(`Success: ${url} (${delay}ms)`);
+		}
+	}, delay);
+};
+
+// Promise version
+const requestPromise = (url) => {
+	return new Promise((resolve, reject) => {
+		const delay = Math.floor(Math.random() * 4500) + 500;
+		setTimeout(() => {
+			if (delay > 2000) {
+				reject('Error: Connection Timeout');
+			} else {
+				resolve(`Success: ${url} (${delay}ms)`);
+			}
+		}, delay);
+	});
+};
+
+requestCallback('movie.com',function(response){
+    console.log('success',response);
+},
+function (error) {
+ console.log('error',error);   
+}
+);
